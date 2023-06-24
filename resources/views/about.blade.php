65a631b3492e9ddd9f8d56e9bad2204339076dde
Adrian Changalombo <alextravc.342@gmail.com>
2023-06-24 10:55:29 -0500

    [ADD]: initial structure + tailwindcss setup

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!-- usar 1 layout -->
+@extends('layouts.app')
+
+
+<!-- Inyectar contenido en el template del q extendemos
+    Son contenedores q se reservan en el template, pero si NO los utilizo NO pasa nada, NO da error
+-->
+@section('title')
+    About Us
+@endsection
+
+
+@section('content')
+    About Us Content
+@endsection
