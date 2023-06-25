555c00f120c0a61fcdbdad5acb6e28667b693e3f
Adrian Changalombo <alextravc.342@gmail.com>
2023-06-24 22:00:54 -0500

    [ADD]: keep session option

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -18,7 +18,8 @@ class LoginController extends Controller
             'password' => 'required|min:6',
         ]);
 
-        if (!auth()->attempt($request->only('email', 'password'))) {
+        // remember para generar token en db y cookie y mantener la session
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
             // whith coloca el message en la SESSION y lo podemos recuperar en la view
             return back()->with('message', 'Incorrect Credentials'); // back a la prev page
         }
