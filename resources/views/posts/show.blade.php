c4b495604525bf439273180269d9749cba887e9e
Adrian Changalombo <alextravc.342@gmail.com>
2023-06-25 16:34:48 -0500

    [ADD]: inital post view design

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,28 @@
     {{ $post->title }}
 @endsection
 
-@hasSection('content')
-    <div class="">
+@section('content')
+    <div class="container mx-auto flex items-center">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post image - {{ $post->title }}">
 
+            <div class="p-3">
+                <p>0 likes</p>
+            </div>
+
+            <div>
+                {{-- x las relaciones entre DB y las convenciones en el name: --}}
+                <p class="font-bold">{{ $post->user->username }}</p>
+                <p class="text-sm text-gray-500">
+                    {{-- gracias a Carbon de Laravel <- difference for humans --}}
+                    {{ $post->created_at->diffForHumans() }}
+                </p>
+                <p class="mt-5">{{ $post->description }}</p>
+            </div>
+        </div>
+
+        <div class="md:w-1/2 p-5">
+            2
+        </div>
     </div>
-@endif
+@endsection
