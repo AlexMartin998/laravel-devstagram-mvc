e4da41a04285a8e97f2cf94e49bc8e70f42fe6a6
Adrian Changalombo <alextravc.342@gmail.com>
2023-06-28 23:02:19 -0500

    [ADD]: laravel components intro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -80,24 +80,7 @@
     <section>
         <h2 class="text-4xl text-center font-black my-10">Publications</h2>
 
-        @if ($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div class="">
-                        {{-- x el route model binding necesita el post, o solo el id --}}
-                        {{-- <a href="{{ route('posts.show', ['user'=>$user, 'post'=>$post]) }}"> --}}
-                        <a href="{{ route('posts.show', [$user->username, $post]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Post image | {{ $post->title }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="my-10">
-                {{-- laravel ya mete la logica de la paginacion | defaul pagining with tailwindcss styles --}}
-                {{ $posts->links() }}
-            </div>
-        @else
-            <p class="text-center">No Posts</p>
-        @endif
+        <x-post-list :posts="$posts" />
+
     </section>
 @endsection
