387e5ed726b98c2fe3d6429705a640f866d47b82
Adrian Changalombo <alextravc.342@gmail.com>
2023-06-28 22:23:37 -0500

    [ADD]: home page design and functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     // si solo va tener 1 method se usa el   __invoke   q se llama solo como el constructor
     public function __invoke()
     {
         // get followings
         $ids = auth()->user()->followings->pluck('id')->toArray();
-        $posts = Post::whereIn('user_id', $ids)->paginate(20);
+        $posts = Post::whereIn('user_id', $ids)->latest()->paginate(20);
 
         return view('home', [
             'posts' => $posts,
